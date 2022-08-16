4be23b1fa439ca59327b8a634ea667d5802e0dc9
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-08-16 13:47:10 +1000

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -1,6 +1,7 @@
-const lithium = Item("lithium")
-lithium.name = "Lithium"
-lithium.color = Color.valueOf("949378")
+lithium = extend(Item, "lithium", {
+    name: "Lithium",
+    color: Color.valueOf("949378")
+})
 const pyroblate = Item("pyroblate")
 pyroblate.name = "Pyroblate"
 pyroblate.color = Color.valueOf("c4883c")
