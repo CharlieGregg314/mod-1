dfb64669412600bec0f96a5c090faf60fdbce663
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-08-16 17:18:42 +1000

    Update create.js

--- scripts/create.js
+++ scripts/create.js
@@ -1,14 +1,1 @@
-function create(obj, base) {
-    if (base === undefined) {
-        return function(base, opts) {
-            let id = opts.name.toLowerCase().replace(" ", "-")
-            obj[id] = extend(base, id, opts)
-        }
-    } else {
-        return function(opts) {
-            let id = opts.name.toLowerCase().replace(" ", "-")
-            obj[id] = extend(base, id, opts)
-        }
-    }
-}
-module.exports = create
+
