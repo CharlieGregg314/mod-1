808d697eee32e7490a0ea39a362094ad17b2a2fe
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-08-16 14:50:21 +1000

    Create create.js

---
+++ scripts/create.js
@@ -0,0 +1,19 @@
+function create(obj, base) {
+    if (base === undefined) {
+        return function(base, {name, ...opts}) {
+            id = name.toLowerCase().replace(" ", "-")
+            obj[id] = extend(base, id, {
+                name,
+                ...opts
+            })
+        }
+    } else {
+        return function({name, ...opts}) {
+            id = name.toLowerCase().replace(" ", "-")
+            obj[id] = extend(base, id, {
+                name,
+                ...opts
+            })
+        }
+    }
+}
