72d9fbef6629a6347eaae080e2fe6401f1b33468
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-08-16 14:15:35 +1000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,20 @@
-function create(obj) {
-    return function({name, ...opts}) {
-        id = name.toLowerCase().replace(" ", "-")
-        obj[id] = extend(Item, id, {
-            name,
-            ...opts
-        })
+function create(obj, base) {
+    if (base === undefined) {
+        return function(base, {name, ...opts}) {
+            id = name.toLowerCase().replace(" ", "-")
+            obj[id] = extend(base, id, {
+                name,
+                ...opts
+            })
+        }
+    } else {
+        return function({name, ...opts}) {
+            id = name.toLowerCase().replace(" ", "-")
+            obj[id] = extend(base, id, {
+                name,
+                ...opts
+            })
+        }
     }
 }
 require("blocks");
