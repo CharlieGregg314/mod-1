987c662925080876c63ad7aca47a64c5d554df7c
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-08-16 16:42:04 +1000

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,10 +1,11 @@
 const items = require("items")
 const blocks = {}
-const create_block = require("create")(blocks)
-create_block(Wall, {
-    name: "Flakite Wall", 
-    health: 8000,
-    requirements: [new ItemStack(items.flakite, 6)],
-    category: Category.wall
-})
+console.log(Block("flakite-wall").category)
+// const create_block = require("create")(blocks)
+// create_block(Wall, {
+//     name: "Flakite Wall", 
+//     health: 8000,
+//     requirements: [new ItemStack(items.flakite, 6)],
+//     category: Category.wall
+// })
 module.exports = blocks
