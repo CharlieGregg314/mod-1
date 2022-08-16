85447a6ffbba6610e178c9212fc8d6bcfd1d0d3d
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-08-16 14:56:48 +1000

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,5 +1,5 @@
-blocks = {}
-create_block = require("create.js").create(blocks)
+const blocks = {}
+const create_block = require("create.js").create(blocks)
 create_block(Wall, {
     name: "Flakite Wall", 
     health: 80,
