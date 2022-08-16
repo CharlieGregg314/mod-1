7187753bd1dff1e3fc4b47ec36e0395eb4ca0dc4
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-08-16 14:07:34 +1000

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -1,7 +1,30 @@
-lithium = extend(Item, "lithium", {
-    name: "Lithium",
-    color: Color.valueOf("949378")
+items = {}
+function create_item({name, ...opts}) {
+    id = name.toLowerCase().replace(" ", "-")
+    items[id] = extend(Item, id, {
+        name,
+        ...opts
+    })
+}
+create_item({
+    name: "Flakite", 
+    color: Color.valueOf("949378"),
+    flammability: 0.2,
+    charge: 0.4,
+    cost: 0.5
 })
-const pyroblate = Item("pyroblate")
-pyroblate.name = "Pyroblate"
-pyroblate.color = Color.valueOf("c4883c")
+create_item({
+    name: "Pyroblate",
+    color: Color.valueOf("c48834"),
+    flammability: 0.8,
+    explosiveness: 0.2,
+    cost: 0.8,
+    healthScaling: 0.2
+})
+create_item({
+    name: "Diricyte",
+    color: Color.valueOf("12c098"),
+    charge: 0.9,
+    cost: 1.3,
+    healthScaling: 0.8
+})
