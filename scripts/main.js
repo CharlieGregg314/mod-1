6b857d05a2a81169bba097ef8e9bec2ef20d4711
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-08-16 14:10:24 +1000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
+function create(obj) {
+    return function({name, ...opts}) {
+        id = name.toLowerCase().replace(" ", "-")
+        obj[id] = extend(Item, id, {
+            name,
+            ...opts
+        })
+    }
+}
 require("blocks");
 require("items");
